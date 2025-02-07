e183946f7198f44a36ef20d024cdc4568c989714
zhangfuxing <zhangfuxing@cmict.chinamobile.com>
2025-02-07 11:17:45 +0800

    update ui_macros

--- crates/ui_macros/src/lib.rs
+++ crates/ui_macros/src/lib.rs
@@ -1,6 +1,20 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse::ParseStream, parse_macro_input, Expr, Result};
+use syn::{parse::Parse, parse::ParseStream, parse_macro_input, Expr, Result};
+
+struct MacroInput {
+    exprs: Vec<Expr>,
+}
+
+impl Parse for MacroInput {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let mut exprs = Vec::new();
+        while !input.is_empty() {
+            exprs.push(input.parse()?);
+        }
+        Ok(MacroInput { exprs })
+    }
+}
 
 fn parse_input(input: ParseStream) -> Result<Vec<Expr>> {
     let mut exprs = Vec::new();
@@ -13,6 +27,7 @@ fn parse_input(input: ParseStream) -> Result<Vec<Expr>> {
 #[proc_macro]
 pub fn col(input: TokenStream) -> TokenStream {
     let exprs = parse_macro_input!(input with parse_input);
+    // let MacroInput { exprs } = parse_macro_input!(input as MacroInput);
 
     let output = exprs.iter().map(|expr| {
         quote! {
@@ -33,7 +48,8 @@ pub fn col(input: TokenStream) -> TokenStream {
 
 #[proc_macro]
 pub fn row(input: TokenStream) -> TokenStream {
-    let exprs = parse_macro_input!(input with parse_input);
+    // let exprs = parse_macro_input!(input with parse_input);
+    let MacroInput { exprs } = parse_macro_input!(input as MacroInput);
 
     let output = exprs.iter().map(|expr| {
         quote! {
